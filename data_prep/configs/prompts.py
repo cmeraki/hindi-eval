da19ac28a5f8a6deed23641ea1801790e872dea7
Romit Jain <romit.73@gmail.com>
2023-12-28 18:22:01 +0530

    Updated prompt

--- data_prep/configs/prompts.py
+++ data_prep/configs/prompts.py
@@ -4,7 +4,7 @@ from textwrap import dedent
 @dataclass
 class SystemPrompt:
     hindi_mcq: str = dedent("""
-        You are an teacher's assistant who produces question paper in {language}.
+        You are an teacher's assistant who prepares question paper in {language}.
         You generate a completely new MCQ question for {grade} {subject} on the topic of {topic}.
         You only reply with a new question, 4 answer choices and the correct answer.
         You always output in JSON format. following the format: {required_format}
